308ceff90cfe2f3723dbcfbac6fdb2c4f71f00a5
xAhmadRaza <Gintoki.sakata789@gmail.com>
2024-03-25 04:32:30 +0500

    feat: add non-responsive hero, add projct section markup

--- script/script.js
+++ script/script.js
@@ -9,3 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
     navBarHeaderEl.classList.toggle("nav__open");
   });
 });
+
+var typeData = new Typed(".role", {
+  strings: [
+    "Full Stack Developer",
+    "Web Developer",
+    "UI-UX Designer",
+    "Backend Developer",
+    "Coder",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
